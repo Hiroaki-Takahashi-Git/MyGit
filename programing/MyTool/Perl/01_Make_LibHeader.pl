0c976920f2741b3ef65c60b1058e6ff0c941ada9
Hiroaki-T <atd00101@bcc.bai.ne.jp>
2019-08-05 21:14:26 +0900

    中身を更に修正

--- programing/MyTool/Perl/01_Make_LibHeader.pl
+++ programing/MyTool/Perl/01_Make_LibHeader.pl
@@ -4,14 +4,15 @@ use File::Path;
 use File::Find;
 
 my	$SRCROOT	=	$ARGV[0];
-my	$VERSION	=	$ARGV[1];
+my	$PREFIXNAME	=	$ARGV[1];
+my	$VERSION	=	$ARGV[2];
 
 my	$ARGC	= scalar(@ARGV);
 
 my	$INCLUDEDIR	=	"$SRCROOT\\include";
 my	$VER_STRING	=	$VERSION;
 
-if ( $ARGC == 2 ) {
+if ( $ARGC == 3 ) {
 	$VER_STRING	=~	s/\.//g;
 } else {
 	$VER_STRING = "";
@@ -38,9 +39,11 @@ sub	MAKE_LIB_HDR {
 	
 }
 
-open(HPP, "> $INCLUDEDIR\\add_libs.hpp") or die "Error!!!";
-print HPP "#ifndef ADD_LIBS_HPP\n";
-print HPP "#define ADD_LIBS_HPP\n";
+my $PREFIX2 = $PREFIXNAME;
+$PREFIX2 =~ s/(\w+)/\U$1/;
+open(HPP, "> $INCLUDEDIR\\${PREFIXNAME}${VER_STRING}_libs.hpp") or die "Error!!!";
+print HPP "#ifndef ${PREFIX2}${VER_STRING}_LIBS_HPP\n";
+print HPP "#define ${PREFIX2}${VER_STRING}_LIBS_HPP\n";
 print HPP "\n";
 print HPP "#if _DEBUG\n";
 foreach my $d_lib (@D_LIB_ARR) {
