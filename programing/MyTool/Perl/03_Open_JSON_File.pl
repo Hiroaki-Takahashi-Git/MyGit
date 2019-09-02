a3a7e2210a434cd445398ac619d433a62febfb16
Hiroaki-T <atd00101@bcc.bai.ne.jp>
2019-09-02 20:24:23 +0900

    Perlプログラムの追加 　・JSONファイルを読み込むスクリプト

--- programing/MyTool/Perl/03_Open_JSON_File.pl
+++ programing/MyTool/Perl/03_Open_JSON_File.pl
@@ -1,14 +1,40 @@
 use utf8;
 use JSON;
 use File::Slurp;
+use Encode qw/encode decode/;
 
-my $JSON_FILE = $ARGV[0];
+# my $JSON_FILE = $ARGV[0];
+my $JSON_DIR = $ARGV[0];
 
-&get_json_contents($JSON_FILE);
+# my $JSON_DATA = &get_json_contents($JSON_FILE);
+
+# print encode('Shift_JIS', $JSON_DATA->{name});
+# print "\n";
+# print encode('Shift_JIS', $JSON_DATA->{parameters}->{HP}->{最大});
 
 sub get_json_contents {
     my ($fname) = @_;
 
-    my $json_data = File::Slurp::read_json_file($fname);
+    print "File : $fname\n";
+
+    my $json_data = File::Slurp::read_file($fname);
     my $json = JSON::decode_json($json_data);
+
+    return ($json);
 }
+
+sub MAIN {
+    my ($DIRPATH) = @_;
+
+    opendir(DIR, "$DIRPATH") or die "Error!!";
+    my @dir_list = readdir(DIR);
+
+    foreach my $subd (@dir_list) {
+        next if ($subd =~ /^\.+$/);
+        next if ($subd !~ /PDMonster\d+/);
+        print "$subd\n";
+    }
+    closedir(DIR);
+}
+
+&MAIN($JSON_DIR);
