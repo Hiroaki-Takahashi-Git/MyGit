e18d48ec666ba7ed411652841a38f442ba2ba387
Hiroaki-T <atd00101@bcc.bai.ne.jp>
2019-11-29 23:10:10 +0900

    [2019/11/29]コミット漏れのファイルを追加

---
+++ programing/Puzzle_And_Dragons/Perl/PD02/PD02_Main.pl
@@ -0,0 +1,13 @@
+use utf8;
+use strict;
+use warnings;
+
+require '.\PDDefine\PD02_Define.pm';
+require '.\PDForm01\PD02_Form01.pm';
+# require '.\PDForm02\PD02_Form02.pm';
+
+&MAIN();
+
+sub MAIN {
+    &Form01_Func01_Main();
+}
