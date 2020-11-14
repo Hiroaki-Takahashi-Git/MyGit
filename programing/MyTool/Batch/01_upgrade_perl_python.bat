4e0951a0af2936e5fa64fa93138d1908a9e6ee8b
Hiroaki-T <atd00101@bcc.bai.ne.jp>
2020-11-14 09:52:24 +0900

    [2020/11/14]Batchプログラムを修正

--- programing/MyTool/Batch/01_upgrade_perl_python.bat
+++ programing/MyTool/Batch/01_upgrade_perl_python.bat
@@ -1,25 +1,25 @@
 @echo off
 setlocal
 rem	set DEVELOPROOT=C:\Develop
-set PERLROOT=C:\Perl
-set PYTHONROOT=C:\Python
-set PYTHON27=%PYTHONROOT%\Python27
-set PYTHON36=%PYTHONROOT%\Python36
+set PERLROOT=C:\Develop\Perl
+set PYTHONROOT=C:\Develop\Python
+set PYTHON2=%PYTHONROOT%\Python2
+set PYTHON3=%PYTHONROOT%\Python3
 
 rem	perlのアップグレード
-ppm upgrade -install
+rem	ppm upgrade -install
 
 rem	pythonのアップグレード
 rem	python2.7
 rem	python2.7のpipのアップグレード
-rem cd /d %PYTHON27%
-rem python -m pip install --upgrade pip
-rem rem	モジュールの一括アップグレード
-rem python -c "import sys;from subprocess import call,check_output;p=[sys.executable,'-m','pip'];[call(p+['install','-U',n.split('=')[0]])for n in['pip']+check_output(p+['freeze']).decode().splitlines()]"
+cd /d %PYTHON2%
+python -m pip install --upgrade pip
+rem	モジュールの一括アップグレード
+python -c "import sys;from subprocess import call,check_output;p=[sys.executable,'-m','pip'];[call(p+['install','-U',n.split('=')[0]])for n in['pip']+check_output(p+['freeze']).decode().splitlines()]"
 
 rem python3.6
 rem	python3.6のpipのアップグレード
-cd /d %PYTHON36%
+cd /d %PYTHON3%
 python -m pip install --upgrade pip
 rem	モジュールの一括アップグレード
 python -c "import sys;from subprocess import call,check_output;p=[sys.executable,'-m','pip'];[call(p+['install','-U',n.split('=')[0]])for n in['pip']+check_output(p+['freeze']).decode().splitlines()]"
